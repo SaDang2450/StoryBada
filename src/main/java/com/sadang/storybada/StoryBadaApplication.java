447b741a9275e3b99b6cda2b31af84665f5552cd
SaDang <SaDang@SaDang>
2025-07-16 17:07:41 +0900

    SaDang  - 이야기바다 프로젝트 시작

---
+++ src/main/java/com/sadang/storybada/StoryBadaApplication.java
@@ -0,0 +1,13 @@
+package com.sadang.storybada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoryBadaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoryBadaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sadang/storybada/StoryBadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sadang.storybada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoryBadaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
