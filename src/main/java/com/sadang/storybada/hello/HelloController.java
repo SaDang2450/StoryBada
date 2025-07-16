38c3eae28ffbe75d827ef2201aae559c20c07770
SaDang <SaDang@SaDang>
2025-07-16 18:51:41 +0900

    SaDang  - #1 데이터 베이스 쿼리 생성 완료  - #1 예시 데이터 밀어넣어야 함

--- src/main/java/com/sadang/storybada/hello/HelloController.java
+++ src/main/java/com/sadang/storybada/hello/HelloController.java
@@ -1,16 +1,16 @@
 package com.sadang.storybada.hello;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/hello")
 public class HelloController {
 
-    @ResponseBody
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String helloWorld() {
-        return "Hello World";
 
+        return "frontpage/frontpage";
     }
 }
