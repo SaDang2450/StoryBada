026636de1a895584f2a2cdc5669bc2d320f113df
SaDang <SaDang@SaDang>
2025-07-22 17:16:49 +0900

    SaDang  - URL 설계 수정 : 캐릭터 전환 페이지 ... /user/character > /user/change  - #2 VIEW 생성 1차 완료. 점검 시작

--- src/main/java/com/sadang/storybada/user/UserController.java
+++ src/main/java/com/sadang/storybada/user/UserController.java
@@ -16,6 +16,25 @@ public class UserController {
 
     @GetMapping("/find")
     public String find_view() {
+
         return "user/find";
+    }
+
+    @GetMapping("/mypage")
+    public String mypage_view() {
+
+        return "user/mypage";
+    }
+
+    @GetMapping("/myhall")
+    public String myhall_view() {
+
+        return "user/myhall";
+    }
+
+    @GetMapping("/change")
+    public String change_view() {
+
+        return "user/change";
     }
 }
