dfb453952ef67d36ca54cab772475e2901b0f00a
SaDang <SaDang@SaDang>
2025-08-05 18:48:45 +0900

    SaDang  - chatGPT 이용한 질문 확인 : ajax 이용 post 방식 부분 새로고침

--- src/main/java/com/sadang/storybada/game/dice/DiceRestController.java
+++ src/main/java/com/sadang/storybada/game/dice/DiceRestController.java
@@ -36,7 +36,7 @@ public class DiceRestController {
 
             return ApiResponse.fail(responseCode);
         }
-
+//
         // betting
         diceBufferService.addDiceBetting(userDTO, betting, hp);
 
